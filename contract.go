43e9cc7e72f5b3c75c1a3b2fd84c698d1837682d
spidertrallo <50380577+spidertrallo@users.noreply.github.com>
2021-04-06 15:16:14 +0200

    Add files via upload

---
+++ contract.go
@@ -0,0 +1,177 @@
+package contracts
+import (
+	"github.com/hyperledger/fabric-chaincode-go/pkg/cid"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+type NewContract struct {
+	contractapi.Contract
+}
+
+type UTXO struct {
+	Key    string `json:"utxo_key"`
+	Owner  string `json:"owner"`
+	Amount int    `json:"amount"`
+}
+
+
+
+func Transfer(ctx contractapi.TransactionContextInterface, utxoInputKeys []string, amount int) (*UTXO, error) {
+	hasOU, err := cid.HasOUValue(ctx.GetStub(), "client1")
+	if err != nil {
+		return nil, err
+	}
+	if !hasOU {
+		return nil, ErrNoFarmer
+	}
+	clientMSPID, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get MSPID: %v", err)
+	}
+	if clientMSPID != "farmerMSP" {
+		return nil, fmt.Errorf("client is not authorized to receive new tokens")
+	}
+
+
+	// Get ID of submitting client identity
+	clientID, err := ctx.GetClientIdentity().GetID()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get client id: %v", err)
+	}
+
+	// Validate and summarize utxo inputs
+	utxoInputs := make(map[string]*UTXO)
+	var totalInputAmount int
+	for _, utxoInputKey := range utxoInputKeys {
+		if utxoInputs[utxoInputKey] != nil {
+			return nil, fmt.Errorf("the same utxo input can not be spend twice")
+		}
+
+		utxoInputCompositeKey, err := ctx.GetStub().CreateCompositeKey("utxo", []string{clientID, utxoInputKey})
+		if err != nil {
+			return nil, fmt.Errorf("failed to create composite key: %v", err)
+		}
+
+		// validate that client has a utxo matching the input key
+		valueBytes, err := ctx.GetStub().GetState(utxoInputCompositeKey)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read utxoInputCompositeKey %s from world state: %v", utxoInputCompositeKey, err)
+		}
+
+		if valueBytes == nil {
+			return nil, fmt.Errorf("utxoInput %s not found for client %s", utxoInputKey, clientID)
+		}
+
+		//amount, _ := strconv.Atoi(string(valueBytes)) // Error handling not needed since Itoa() was used when setting the utxo amount, guaranteeing it was an integer.
+
+		utxoInput := &UTXO{
+			Key:    utxoInputKey,
+			Owner:  clientID,
+			Amount: amount,
+		}
+
+		totalInputAmount += amount
+		utxoInputs[utxoInputKey] = utxoInput
+	}
+
+	for _, utxoInput := range utxoInputs {
+
+		utxoInputCompositeKey, err := ctx.GetStub().CreateCompositeKey("utxo", []string{utxoInput.Owner, utxoInput.Key})
+		if err != nil {
+			return nil, fmt.Errorf("failed to create composite key: %v", err)
+		}
+
+		err = ctx.GetStub().DelState(utxoInputCompositeKey)
+		if err != nil {
+			return nil, err
+	}
+	log.Printf("utxoInput deleted: %+v", utxoInput)
+	}
+
+	utxoOutput:=new(UTXO)
+	utxoOutput.Key = ctx.GetStub().GetTxID() + ".0"
+	mspid:="originatorMSP"
+	utxoOutput.Owner=mspid
+	utxoOutput.Amount=totalInputAmount
+	utxoOutputCompositeKey, err := ctx.GetStub().CreateCompositeKey("utxo", []string{utxoOutput.Owner, utxoOutput.Key})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create composite key: %v", err)
+	}
+
+	err = ctx.GetStub().PutState(utxoOutputCompositeKey, []byte(strconv.Itoa(utxoOutput.Amount)))
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("utxoOutput created: %+v", utxoOutput)
+
+	return utxoOutput, nil
+}
+
+//ejemplo fabric-samples
+// ClientUTXOs returns all UTXOs owned by the calling client
+//cambiar a interfaz
+func ClientUTXOs(ctx contractapi.TransactionContextInterface) ([]string, error) {
+
+	// Get ID of submitting client identity
+	clientID, err := ctx.GetClientIdentity().GetID()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get client id: %v", err)
+	}
+
+	// since utxos have a composite key of owner:utxoKey, we can query for all utxos matching owner:*
+	utxoResultsIterator, err := ctx.GetStub().GetStateByPartialCompositeKey("utxo", []string{clientID})
+	if err != nil {
+		return nil, err
+	}
+	defer utxoResultsIterator.Close()
+
+	var newUTXO UTXO
+	var utxos []*UTXO
+	for utxoResultsIterator.HasNext() {
+		utxoRecord, err := utxoResultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		// composite key is expected to be owner:utxoKey
+		_, compositeKeyParts, err := ctx.GetStub().SplitCompositeKey(utxoRecord.Key)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(compositeKeyParts) != 2 {
+			return nil, fmt.Errorf("expected composite key with two parts (owner:utxoKey)")
+		}
+
+		utxoKey := compositeKeyParts[1] // owner is at [0], utxoKey is at[1]
+
+		if utxoRecord.Value == nil {
+			return nil, fmt.Errorf("utxo %s has no value", utxoKey)
+		}
+
+		amount, _ := strconv.Atoi(string(utxoRecord.Value)) // Error handling not needed since Itoa() was used when setting the utxo amount, guaranteeing it was an integer.
+
+		utxo := &UTXO{
+			Key:    utxoKey,
+			Owner:  clientID,
+			Amount: amount,
+		}
+		newUTXO.Key=utxoKey
+		newUTXO.Amount=amount
+		utxos = append(utxos, utxo)
+	}
+	return []string{newUTXO.Key,strconv.Itoa(newUTXO.Amount)}, nil
+}
+
+
+func AfterTransaction(ctx contractapi.TransactionContextInterface) error{
+	idUTXO,err:=s.ClientUTXOs(ctx)
+	if err!=nil{
+		return fmt.Errorf("Error: %v",err)
+	}
+	value:=idUTXO[0]
+	log.Printf("valor de la clave: %v",value)
+	cant,_:=strconv.Atoi(idUTXO[1])
+	_,err=s.Transfer(ctx,[]string{value},cant)
+	return err
+}
